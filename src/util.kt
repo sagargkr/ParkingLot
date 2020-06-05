b7c19f2fcf180a5657707585a14872569bffb5ba
Sagar Gupta <sagargupta123kr@gmail.com>
2020-06-05 17:17:07 +0530

    Added bike Loop

--- src/util.kt
+++ src/util.kt
@@ -87,3 +87,71 @@ fun carParkingLoop() {
         }
     }
 }
+
+fun bikeParkingLoop() {
+
+    loop2@ while (true) {
+        println(">")
+        when (reader.next()) {
+            "create" -> {
+                val n = reader.nextInt()
+                bikeParking = VehicleParking(n, "car")
+                println("Created car parking lot with $n spots.")
+            }
+            "park" -> {
+                if (bikeParking.amountOfSpotsAvailable()) {
+                    println("Sorry, a parking lot has not been created.")
+                    continue@loop2
+                }
+                val number = reader.next()
+                val color = reader.next()
+                val car = Car(number, color.toLowerCase())
+                val parkedSpot = bikeParking.park(car)
+                if (parkedSpot == 0) {
+                    println("Sorry, the parking lot is full.")
+                } else println("${color.capitalize()} car parked in spot $parkedSpot.")
+            }
+            "leave" -> {
+                if (bikeParking.amountOfSpotsAvailable()) {
+                    println("Sorry, a parking lot has not been created.")
+                    continue@loop2
+                }
+                val spot = reader.nextInt()
+                val result = bikeParking.leave(spot)
+                if (result) println("Spot $spot is free.")
+                else {
+                    println("There is no car in the spot $spot.")
+                }
+            }
+            "status" -> {
+                if (bikeParking.amountOfSpotsAvailable()) {
+                    println("Sorry, a parking lot has not been created.")
+                    continue@loop2
+                }
+                bikeParking.parkedVehicle()
+            }
+            "reg_by_color" -> {
+                if (bikeParking.amountOfSpotsAvailable()){
+                    println("Sorry, a parking lot has not been created.")
+                    continue@loop2
+                }
+                bikeParking.regByColor(reader.next())
+            }
+            "spot_by_color" -> {
+                if (bikeParking.amountOfSpotsAvailable()){
+                    println("Sorry, a parking lot has not been created.")
+                    continue@loop2
+                }
+                bikeParking.spotByColor(reader.next())
+            }
+            "spot_by_reg"->{
+                if (bikeParking.amountOfSpotsAvailable()){
+                    println("Sorry, a parking lot has not been created.")
+                    continue@loop2
+                }
+                bikeParking.spotByReg(reader.next())
+            }
+            "exit" -> break@loop2
+        }
+    }
+}
