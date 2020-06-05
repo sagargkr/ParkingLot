7232f38d44d6b0154bd1d0f3f9b647cf54914022
Sagar Gupta <sagargupta123kr@gmail.com>
2020-06-05 18:44:28 +0530

    Updated -> LICENSE text

--- src/util.kt
+++ src/util.kt
@@ -1,3 +1,26 @@
+/* MIT License
+
+Copyright (c) 2020 Sagar Gupta
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 import java.util.*
 
 var carParking = VehicleParking()
@@ -105,8 +128,8 @@ fun bikeParkingLoop() {
                 }
                 val number = reader.next()
                 val color = reader.next()
-                val car = Car(number, color.toLowerCase())
-                val parkedSpot = bikeParking.park(car)
+                val bike = Bike(number, color.toLowerCase())
+                val parkedSpot = bikeParking.park(bike)
                 if (parkedSpot == 0) {
                     println("Sorry, the parking lot is full.")
                 } else println("${color.capitalize()} car parked in spot $parkedSpot.")
@@ -173,8 +196,8 @@ fun scooterParkingLoop() {
                 }
                 val number = reader.next()
                 val color = reader.next()
-                val car = Car(number, color.toLowerCase())
-                val parkedSpot = scooterParking.park(car)
+                val scooter = Scooter(number, color.toLowerCase())
+                val parkedSpot = scooterParking.park(scooter)
                 if (parkedSpot == 0) {
                     println("Sorry, the parking lot is full.")
                 } else println("${color.capitalize()} car parked in spot $parkedSpot.")
